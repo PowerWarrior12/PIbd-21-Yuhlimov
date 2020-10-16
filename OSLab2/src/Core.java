ae7063b10978d7347552e50e353d527ef336bc85
PowerWarrior12 <61107321+PowerWarrior12@users.noreply.github.com>
2020-10-16 15:25:41 +0400

    Add files via upload

--- OSLab2/src/Core.java
+++ OSLab2/src/Core.java
@@ -3,17 +3,17 @@ import java.util.ArrayDeque;
 import java.util.Random;
 
 public class Core {
-    ArrayDeque<Process> arrProcess = new ArrayDeque<>();
-    Random rand = new Random();
+    private ArrayDeque<Process> arrProcess = new ArrayDeque<>();
+    private Random rand = new Random();
     private final int time = 7;
 
     public void createProcess() {
         for (int i = 0; i < 3 + rand.nextInt(10); i++) {
         	Process pr = new Process(i);
             pr.createFlows();
-            System.out.println("Process " + pr.id);
-            for (int j = 0; j < pr.flows.size(); j++) {
-                System.out.println("\tFlow " + pr.flows.get(j).id + " time: " + pr.flows.get(j).time);
+            System.out.println("Process " + pr.getId());
+            for (int j = 0; j < pr.getFlows().size(); j++) {
+                System.out.println("\tFlow " + pr.getFlows().get(j).getId() + " time: " + pr.getFlows().get(j).getTime());
             }
             arrProcess.add(pr);
         }
@@ -23,23 +23,23 @@ public class Core {
     public void planProcess() {
         while (!arrProcess.isEmpty()) {
         	Process pr = arrProcess.pop();
-            for (int j = 0; j < pr.flows.size(); j++) {
-                int flowId = pr.flows.get(j).id;
-                int processId = pr.id;
-                int flowTime = pr.flows.get(j).time;
+            for (int j = 0; j < pr.getFlows().size(); j++) {
+                int flowId = pr.getFlows().get(j).getId();
+                int processId = pr.getId();
+                int flowTime = pr.getFlows().get(j).getTime();
                 System.out.println("Flow " + flowId + " in process " + processId + " started");
                 if (flowTime - time > 0) {
-                	pr.flows.get(j).changeTime(time);
+                	pr.getFlows().get(j).changeTime(time);
                 	arrProcess.add(pr);
                     System.out.println("\tFlow " + flowId + " in process " + processId + " aborted, need time: "+flowTime +  " given time: "+time);
                 }
                 if (flowTime - time <= 0) {
-                	pr.flows.get(j).flowDone();
-                	pr.flows.remove(j);
+                	pr.getFlows().get(j).flowDone();
+                	pr.getFlows().remove(j);
                     j--;
                 }
             }
-            if (pr.flows.isEmpty()) {
+            if (pr.getFlows().isEmpty()) {
                 pr.processDone();
             }
         }
