c9c2e8935d78b14e5d6a4e9bcbf956e494c1c2f7
PowerWarrior12 <61107321+PowerWarrior12@users.noreply.github.com>
2020-12-07 13:13:05 +0400

    Add files via upload

---
+++ OSLab5/src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+
+    public static void main(String[] args) throws CloneNotSupportedException {
+        Core core = new Core();
+        core.createProcesses();
+        core.processesPlanningWithoutBlocking();
+        core.processesPlanningWithBlocking();
+    }
+}
