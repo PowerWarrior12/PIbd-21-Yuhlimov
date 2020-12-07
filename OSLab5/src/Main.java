731ae42aee38ff1559eb122d7e6c81b35329246c
PowerWarrior12 <61107321+PowerWarrior12@users.noreply.github.com>
2020-12-07 13:11:49 +0400

    Delete Main.java

--- OSLab5/src/Main.java
+++
@@ -1,9 +0,0 @@
-public class Main {
-
-    public static void main(String[] args) throws CloneNotSupportedException {
-        Core core = new Core();
-        core.createProcesses();
-        core.processesPlanningWithoutBlocking();
-        core.processesPlanningWithBlocking();
-    }
-}
